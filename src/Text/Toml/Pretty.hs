cd8d58592278bdefcf5af4cf45a56d81c40aacc5
Joel Taylor <me@joelt.io>
2014-07-09 14:05:01 -0700

    missed a backslash

--- src/Text/Toml/Pretty.hs
+++ src/Text/Toml/Pretty.hs
@@ -22,7 +22,7 @@ import Text.PrettyPrint.ANSI.Leijen
 -- [owner]
 -- name = "Tom Preston-Werner"
 -- organization = \"GitHub\"
--- bio = "GitHub Cofounder & CEO\nLikes tater tots and beer."
+-- bio = "GitHub Cofounder & CEO\\nLikes tater tots and beer."
 -- dob = 1979-05-27T07:32:00Z # First class dates? Why not?
 --
 -- [database]
